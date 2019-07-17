14d22dc0a8c0d43be87543845a2ea8b67b7d4b45
brianjhawkins <37080430+brianjhawkins@users.noreply.github.com>
2019-07-17 13:33:30 -0500

    Add files via upload

---
+++ Assets/Player2D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2D : MonoBehaviour {
+
+    Rigidbody2D rbody;
+    Vector2 velocity;
+
+	void Start () {
+        rbody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * 10;
+	}
+
+    void FixedUpdate()
+    {
+        rbody.MovePosition(rbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Player3D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3D : MonoBehaviour {
+
+    Rigidbody rbody;
+    Vector3 velocity;
+
+	void Start () {
+        rbody = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        velocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized * 10;
+	}
+
+    void FixedUpdate()
+    {
+        rbody.MovePosition(rbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
